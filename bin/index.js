ae81d39fb94818921c4383e05c368e3ca0dba484
dperrymorrow <dperrymorrow@gmail.com>
2017-12-07 18:04:29 -0800

    cli

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+"use strict";
+
+const args = process.argv;
+const git = require("../lib/git");
+const chalk = require("chalk");
+const _ = require("../lib/util/lodash");
+
+const chosenCmd = _.last(args);
+
+const cmds = {
+  async status() {
+    const status = await git.status();
+    console.log(status);
+  },
+};
+
+if (chosenCmd in cmds) cmds[chosenCmd]();
+else console.log(Object.keys(cmds).sort());
