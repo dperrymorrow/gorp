8050e823fada1b989a4ae1735fda256572bd4fbc
dperrymorrow <dperrymorrow@gmail.com>
2018-03-15 13:16:53 -0700

    using simple-git wherever is possible

--- bin/index.js
+++ bin/index.js
@@ -10,35 +10,40 @@ const chalk = require("chalk");
 const args = process.argv;
 const flag = _.last(args);
 
-cleanUp();
-
-async function run(cmd = null) {
-  const choices = await prompts();
-  const match = choices.find(choice => choice.cmd === cmd);
-  if (match) run(await match.value());
-  else listCmds();
-}
-
-async function listCmds() {
-  const currentBranch = await term.branch.current();
-  const choices = await prompts();
-  const color = (await term.status.allClean()) ? "green" : "red";
-
-  const answer = await quiz.prompt({
-    name: "task",
-    pageSize: 50,
-    type: "list",
-    message: "Choose a command",
-    choices,
-    prefix: chalk[color].bold.italic(currentBranch),
-  });
-
-  run(await answer.task());
-}
-
 process.on("unhandledRejection", (reason, p) => {
   console.log(chalk.red(reason), p);
   process.exit();
 });
 
-run(flag);
+cleanUp();
+
+try {
+  async function run(cmd = null) {
+    const choices = await prompts();
+    const match = choices.find(choice => choice.cmd === cmd);
+    if (match) run(await match.value());
+    else listCmds();
+  }
+
+  async function listCmds() {
+    const currentBranch = await term.branch.current();
+
+    const choices = await prompts();
+    const color = (await term.status.allClean()) ? "green" : "red";
+
+    const answer = await quiz.prompt({
+      name: "task",
+      pageSize: 50,
+      type: "list",
+      message: "Choose a command",
+      choices,
+      prefix: chalk[color].bold.italic(currentBranch),
+    });
+
+    run(await answer.task());
+  }
+
+  run(flag);
+} catch (err) {
+  console.log(err);
+}
