90e5361ea070a079b352ffb1ba4d6f84d57f4ccb
dperrymorrow <dperrymorrow@gmail.com>
2018-03-15 15:34:00 -0700

    cleaning up deps

--- bin/index.js
+++ bin/index.js
@@ -4,7 +4,6 @@
 const _ = require("../lib/util/lodash");
 const quiz = require("inquirer");
 const term = require("../lib/terminal");
-const cleanUp = require("node-cleanup");
 const prompts = require("../lib/prompt");
 const chalk = require("chalk");
 const args = process.argv;
@@ -15,35 +14,29 @@ process.on("unhandledRejection", (reason, p) => {
   process.exit();
 });
 
-cleanUp();
-
-try {
-  async function run(cmd = null) {
-    const choices = await prompts();
-    const match = choices.find(choice => choice.cmd === cmd);
-    if (match) run(await match.value());
-    else listCmds();
-  }
-
-  async function listCmds() {
-    const currentBranch = await term.branch.current();
-
-    const choices = await prompts();
-    const color = (await term.status.allClean()) ? "green" : "red";
-
-    const answer = await quiz.prompt({
-      name: "task",
-      pageSize: 50,
-      type: "list",
-      message: "Choose a command",
-      choices,
-      prefix: chalk[color].bold.italic(currentBranch),
-    });
-
-    run(await answer.task());
-  }
-
-  run(flag);
-} catch (err) {
-  console.log(err);
+async function run(cmd = null) {
+  const choices = await prompts();
+  const match = choices.find(choice => choice.cmd === cmd);
+  if (match) run(await match.value());
+  else listCmds();
 }
+
+async function listCmds() {
+  const currentBranch = await term.branch.current();
+
+  const choices = await prompts();
+  const color = (await term.status.allClean()) ? "green" : "red";
+
+  const answer = await quiz.prompt({
+    name: "task",
+    pageSize: 50,
+    type: "list",
+    message: "Choose a command",
+    choices,
+    prefix: chalk[color].bold.italic(currentBranch),
+  });
+
+  run(await answer.task());
+}
+
+run(flag);
