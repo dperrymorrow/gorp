43d1fdce6f939f6c9ce8e80cddf62af61fb20bd1
dperrymorrow <dperrymorrow@gmail.com>
2018-03-16 12:15:48 -0700

    caching and intro prompt formatting

--- bin/index.js
+++ bin/index.js
@@ -7,6 +7,7 @@ const prompts = require("../lib/prompt");
 const chalk = require("chalk");
 const args = process.argv;
 const flag = args.pop();
+const info = require("../package.json");
 
 process.on("unhandledRejection", (reason, p) => {
   console.log(chalk.red(reason), p);
@@ -14,17 +15,46 @@ process.on("unhandledRejection", (reason, p) => {
 });
 
 async function run(cmd = null) {
+  await summary();
   const choices = await prompts();
   const match = choices.find(choice => choice.cmd === cmd);
   if (match) run(await match.value());
   else listCmds();
 }
 
+async function summary() {
+  const dirtyCount = await term.status.dirtyCount();
+  const stagedCount = await term.status.stagedCount();
+  const ahead = await term.status.ahead();
+  const behind = await term.status.behind();
+  const current = await term.branch.current();
+
+  console.log("===================================================");
+
+  console.log("  Gorp:", info.version, "| Branch:", chalk.bold.green(current));
+
+  if (ahead === 0 && behind == 0 && stagedCount === 0 && dirtyCount === 0) {
+    console.log(chalk.green.bold("  All clean & up to date"));
+  } else {
+    console.log(
+      "  Changes:",
+      chalk.red(dirtyCount),
+      "Staged:",
+      chalk.green(stagedCount),
+      "behind:",
+      chalk.red(behind),
+      "Ahead:",
+      chalk.green(ahead)
+    );
+  }
+
+  console.log("===================================================");
+}
+
 async function listCmds() {
   const currentBranch = await term.branch.current();
 
   const choices = await prompts();
-  const color = (await term.status.allClean()) ? "green" : "red";
 
   const answer = await quiz.prompt({
     name: "task",
@@ -32,7 +62,6 @@ async function listCmds() {
     type: "list",
     message: "Choose a command",
     choices,
-    prefix: chalk[color].bold.italic(currentBranch),
   });
 
   run(await answer.task());
